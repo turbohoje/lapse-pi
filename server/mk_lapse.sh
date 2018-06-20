4bd28f57790602e9e2dd5db02501cc73b897b150
Justin <turbohoje@haus.domain>
2018-06-20 04:19:26 -0600

    archive to NAS, process locally

--- server/mk_lapse.sh
+++ server/mk_lapse.sh
@@ -2,17 +2,27 @@
 
 #apt get ffmpeg imagemagick
 
-IMGDIR=/mnt/terra/media/marmot/archive/
+IMGDIR=~/lapse-pi/archive/
 OUTDIR=~/lapse-pi/video-out/imgcache
+BAKDIR=/mnt/terra/media/marmot/archive
 
-MAX=$((60*24))
-#MAXCMD="| tail -${MAX}"
+rsync -azvh --no-o --no-g ${IMGDIR} ${BAKDIR}
+
+MAX=$((60*12))
 rm -rf ${OUTDIR}
 
 mkdir -p ${OUTDIR}
+
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo "** Trapped CTRL-C"
+	exit;
+}
+
 COUNT=1
-TOTAL=`find ${IMGDIR} -name "*.jpg" ${MAXCMD} | wc -l`
-for FILE in `find ${IMGDIR} -name "*.jpg" | sort ${MAXCMD} `; do \
+TOTAL=`find ${IMGDIR} -name "*.jpg" | tail -${MAX} | wc -l`
+for FILE in `find ${IMGDIR} -name "*.jpg" | sort | tail -${MAX} `; do \
   	echo "Processing file ${COUNT}/${TOTAL}"
 	FILENAME=$(printf 'G%07d.JPG' $COUNT)
 	mogrify -resize 1280x720^ -gravity south -crop 1280x650+0+0 +repage -write ${OUTDIR}/${FILENAME} $FILE
