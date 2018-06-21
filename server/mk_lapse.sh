b2e16dbf3dc445fea4d24f27c902e1c491a9c215
Justin <turbohoje@haus.domain>
2018-06-21 06:33:58 -0600

    size of vid change

--- server/mk_lapse.sh
+++ server/mk_lapse.sh
@@ -2,13 +2,17 @@
 
 #apt get ffmpeg imagemagick
 
+# crontab
+# 0 * * * * run-one /home/turbohoje/lapse-pi/server/mk_lapse.sh
+
 IMGDIR=~/lapse-pi/archive/
 OUTDIR=~/lapse-pi/video-out/imgcache
+VIDDIR=~/lapse-pi/video-out
 BAKDIR=/mnt/terra/media/marmot/archive
 
 rsync -azvh --no-o --no-g ${IMGDIR} ${BAKDIR}
 
-MAX=$((60*12))
+MAX=$((60*24))
 rm -rf ${OUTDIR}
 
 mkdir -p ${OUTDIR}
@@ -26,8 +30,10 @@ for FILE in `find ${IMGDIR} -name "*.jpg" | sort | tail -${MAX} `; do \
   	echo "Processing file ${COUNT}/${TOTAL}"
 	FILENAME=$(printf 'G%07d.JPG' $COUNT)
 	mogrify -resize 1280x720^ -gravity south -crop 1280x650+0+0 +repage -write ${OUTDIR}/${FILENAME} $FILE
-	COUNT=$((COUNT+1))
+	if [[ $? == 0 ]]; then
+		COUNT=$((COUNT+1))
+	fi
 done
 
 cd ${OUTDIR}
-ffmpeg -start_number 1 -i G%07d.JPG -c:v libx264 -pix_fmt yuv420p video.mp4
+ffmpeg -start_number 1 -i G%07d.JPG -c:v libx264 -pix_fmt yuv420p ${VIDDIR}/video.mp4
