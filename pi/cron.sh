ec2f6d6de8fc1429aadd5abd584e182d9dc85eba
justin <justin@rocketscience.cc>
2025-10-29 19:35:02 -0600

    staging changes

--- pi/cron.sh
+++ pi/cron.sh
@@ -48,7 +48,7 @@ gsutil cp ${BASEPATH}/thumb.jpg  gs://tlco-public/thumb.jpg
 
 #2nd cam
 mkdir -p "${BASEPATH}/archive/1/${DATE}/"
-curl "https://10.42.0.35/cgi-bin/api.cgi?cmd=Snap&channel=0&user=$UN&password=$PW" -s --insecure --output ${BASEPATH}/archive/1/${DATE}/${TIMESTAMP}.jpg
+curl "https://10.42.0.67/cgi-bin/api.cgi?cmd=Snap&channel=0&user=$UN&password=$PW" -s --insecure --output ${BASEPATH}/archive/1/${DATE}/${TIMESTAMP}.jpg
 /usr/bin/mogrify -compress JPEG2000 -quality 90   ${BASEPATH}/archive/1/${DATE}/${TIMESTAMP}.jpg
 cp ${BASEPATH}/archive/1/${DATE}/${TIMESTAMP}.jpg ${BASEPATH}/box.jpg
 
