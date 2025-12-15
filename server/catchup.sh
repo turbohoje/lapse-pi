8cb03538272a56ffc53f73e5030213a13b2ba4ad
Justin <justin@rocketscience.cc>
2025-12-14 19:23:21 -0700

    catchup changes to use cron script

--- server/catchup.sh
+++ server/catchup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
-DAYS=7
+DAYS=5
 
 set -e
 
 for ((i=$DAYS; i>=1; i--))
 do
   echo ""
-  echo "./mklapse.sh --days=$i"
+  echo "./cron.sh --days=$i"
   sleep 2
-  ./mklapse.sh --days=$i
+  ./cron.sh --days=$i
   #./mklapse.sh --cam=1 --gcs=1 --upload=0 --gen=1 --days=$i
 done
