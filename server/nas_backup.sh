bb4b985994f84b67c23f54df655d6d0b138fbe08
Justin <justin@rocketscience.cc>
2026-04-17 15:15:44 -0600

    change to save disk

--- server/nas_backup.sh
+++ server/nas_backup.sh
@@ -2,7 +2,7 @@
 
 #$ ssh-copy-id -i ~/.ssh/mykey user@host
 cd /home/turbohoje/lapse-pi
-find ./archive -mtime +2 > manifest.txt
+find ./archive -mtime +1 > manifest.txt
 #rsync -avz --no-o --no-g --files-from=manifest.txt turbohoje@10.22.14.3:/var/services/homes/turbohoje/lapse-pi/ # --remove-source-files
 rsync  -avz --files-from=manifest.txt ./ turbohoje@10.22.14.3:/var/services/homes/turbohoje/lapse-pi --remove-source-files
 
